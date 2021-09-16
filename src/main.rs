f3994c5a877f0a73ebabd5a0dca9f3076f1a5290
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-09-16 10:20:23 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+mod p1;
+use p1::Solution;
 fn main() {
-    println!("Hello, world!");
+    let nums = vec![2,7,11,15];
+    let target = 9;
+    let s = Solution::two_sum(nums, target);
+    println!("{:?}", s);
 }
