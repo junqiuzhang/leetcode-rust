fe59b86aba35e935501c5d303d2a18945564c5df
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-09-16 11:20:22 +0800

    7

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-mod p1;
-use p1::Solution;
+mod p7;
+use p7::Solution;
 fn main() {
-    let nums = vec![2,7,11,15];
-    let target = 9;
-    let s = Solution::two_sum(nums, target);
+    let x = 1534236469;
+    let s = Solution::reverse(x);
     println!("{:?}", s);
 }
---
+++ src/p7.rs
@@ -0,0 +1,21 @@
+pub struct Solution {
+}
+impl Solution {
+    pub fn reverse(x: i32) -> i32 {
+        let mut res: i32 = 0;
+        let mut temp = x;
+        while temp != 0 {
+            let (_, boo) = res.overflowing_mul(10);
+            if boo {
+                return 0;
+            }
+            let (_, boo) = (res * 10).overflowing_add(temp % 10);
+            if boo {
+                return 0;
+            }
+            res = res * 10 + temp % 10;
+            temp = temp / 10;
+        }
+        return res;
+    }
+}
